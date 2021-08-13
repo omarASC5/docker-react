60db5a6d40f9cec526510131aa4fa9c5168653a5
Omar Cobas <omarlcobas@gmail.com>
2021-08-13 09:25:58 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // dev/build: 
+    // docker run -p 3000:3000 -v /app/node_modules -v $(pwd):/app containerHash
+    // docker-compose up
+  // tests:
+    // solution 1: docker exec -it containerHashWithVolumes npm run test
+    // solution 2: add it to docker-compose in a new service
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/Asp.js</code> and save to reload.NAH
         </p>
         <a
           className="App-link"
